00086abb3a7a0c67535aff9730355c82811a392a
rafae <rafaelgf@gmail.com>
2020-09-30 13:42:40 -0300

    Exercícios obrigatórios

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,63 @@
+// 1
+// O código testa se a divisão do número inserido pelo usuário pelo número 2 é exata ou não
+// Caso o número inserido pelo usuário seja par, a resposta será "Passou no teste". Caso seja ímpar, a resposta será "Não passou no teste"
+
+// 2
+// a) Informa o preço da fruta digitada pelo usuário
+// b) 2.25
+// c) 5
+
+// 3
+// a) Atribuindo o valor digitado pelo a uma variável do tipo número
+// b) Se digitar 10, aparecerá a mensagem "Esse número passou no teste". Se digitar -10, esta mensagem não aparecerá.
+// c) Sim, pois a variável foi iniciada dentro do escopo do if e chamada fora dele.
+
+// 4
+// let idade = Number(prompt("Qual é a sua idade?"))
+
+// if (idade>=18) {
+//     console.log("Você pode dirigir")
+// } else {
+//     console.log("Você NÃO pode dirigir")
+// }
+
+// 5
+// let turno = prompt("Digite a letra inicial do seu turno: M (matutino) ou V (Vespertino) ou N (Noturno)")
+
+// if (turno==='M') {
+//     console.log("Bom Dia!")
+// }
+// if (turno==='V') {
+//     console.log("Boa Tarde!")
+// }
+// if (turno==='N') {
+//     console.log("Boa Noite!")
+// }
+
+// 6
+// let turno = prompt("Digite a letra inicial do seu turno: M (matutino) ou V (Vespertino) ou N (Noturno)")
+// turno=turno.toLowerCase()
+
+// switch (turno) {
+//     case 'm':
+//         console.log("Bom Dia!")
+//         break
+//     case 'v':
+//         console.log("Boa Tarde!")
+//         break
+//     case 'n':
+//         console.log("Boa Noite!")
+//         break
+// }
+
+// 7
+// let generoDoFilme = prompt("Qual é o gênero do filme?").toLowerCase()
+// let precoDoIngresso = Number(prompt("Qual é o preço do ingresso?"))
+
+// if (generoDoFilme==='fantasia' && precoDoIngresso<=15) {
+//     console.log("Bom filme!")
+// } else {
+//     console.log("Escolha outro filme :(")
+// }
+
+
