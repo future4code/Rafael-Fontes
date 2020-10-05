2a4ddefa61bd51fb183f5b81d73cfa182ec0a4b9
rafae <rafaelgf@gmail.com>
2020-10-05 14:34:16 -0300

    Interpretação

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,19 @@
+// 1
+// a.
+// 10
+// 50
+// b. Uma variável receberia os valores retornados da função. Nada apareceria no console.
+
+// 2
+// a.
+// "Darvas"
+// "Caio"
+// b.
+// "Amanda"
+// "Caio"
+
+// 3
+// Seleciona os números pares de uma array, eleva estes números ao quadrado e cria uma nova array com os resultados das operações matemáticas
+// Nome sugerido: paresAoQuadrado
+
+// 4
