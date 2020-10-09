ac55eee3bb25aa7c03718c2791720d2c3c52e0fe
rafae <rafaelgf@gmail.com>
2020-10-09 14:28:58 -0300

    Interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+// INTERPRETAÇÃO DE CÓDIGO
+
+// 1. A função recebe um valor de 100 e multiplica pela cotação perguntada ao usuário. Retorna este valor para a variável meuDinheiro e a imprime no console
+
+// 2. A função recebe valores numéricos e calcula a rentabilidade conforme o tipo de investimento informado. Retorna estes valores calculados e os imprime no console
+
+// 3. Percorre o array numeros e acrescenta os números pares no array1 e os números ímpares no array2. Depois imoprime o tamanho de cada array no console
+
+// 4. Seleciona o maior e o menor número da array e os imprime no console
+
+
+// LÓGICA DE PROGRAMAÇÃO
+
+// 1. 
