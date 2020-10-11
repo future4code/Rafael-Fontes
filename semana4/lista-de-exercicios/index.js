902d5a73a3c8a63f8f0eaa13adf455f400bf764a
rafae <rafaelgf@gmail.com>
2020-10-11 14:02:41 -0300

    Funções de Array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -193,3 +193,164 @@
 // FUNÇÕES DE ARRAY
 
 // 1
+// const pessoas = [
+// 	{ nome: "Pedro", idade: 20 },
+// 	{ nome: "João", idade: 10 },
+// 	{ nome: "Paula", idade: 12 },
+// 	{ nome: "Artur", idade: 89 } 
+// ]
+
+// // a
+// const soAdultos = pessoas.filter((pessoa) => {
+//     if (pessoa.idade>=20){
+//         return pessoas
+//     }
+// })
+
+// // b
+// const soCriancas = pessoas.filter((pessoa) => {
+//     if (pessoa.idade<20){
+//         return pessoas
+//     }
+// })
+ 
+// console.log(soAdultos)
+// console.log(soCriancas)
+
+// 2
+// const array = [1, 2, 3, 4, 5, 6]
+
+// // a
+// const arrayX2 = []
+// for (i=0; i<array.length; i++) {
+//     arrayX2[i]=array[i]*2
+// }
+// console.log(arrayX2)
+
+// // // b
+// const arrayX3 = []
+// for (i=0;i<array.length; i++) {
+//     arrayX3[i]=array[i]*3
+//     arrayX3[i]=`"${arrayX3[i]}"`
+// }
+// console.log(arrayX3)
+
+// // // c
+// const arrayParidade = []
+// for (i=0;i<array.length;i++) {
+//     if (array[i]%2===0) {
+//         arrayParidade[i]=`"${array[i]} é par"`
+//     } else {
+//         arrayParidade[i]=`"${array[i]} é ímpar"`
+//     }
+// }
+// console.log(arrayParidade)
+
+// // 3
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+// // a
+// const permitidas = pessoas.filter((pessoa) => {
+//     if (pessoa.altura>1.5 && (pessoa.idade>14 && pessoa.idade<60)) {
+//         return true
+//     }
+// })
+// console.log(permitidas)
+
+// // b
+// const naoPermitidas = pessoas.filter((pessoa) => {
+//     if (pessoa.altura<=1.5 || (pessoa.idade<=14 || pessoa.idade>=60)) {
+//         return true
+//     }
+// })
+// console.log(naoPermitidas)
+
+// 4
+// const consultas = [
+// 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+// 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+// 	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+// 	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+// ]
+
+// const canceladas = consultas.filter ((consulta) => {
+//     if (consulta.cancelada) {
+//         return consulta
+//     }
+// })
+
+// const confirmadas = consultas.filter ((consulta) => {
+//     if (consulta.cancelada===false) {
+//         return consulta
+//     }
+// })
+
+// let emailCanceladas = []
+// let emailConfirmadas = []
+// let pronome
+// let lembrar
+// for (i=0; i<canceladas.length;i++) {
+//     if (canceladas[i].genero==="masculino"){
+//         pronome="Sr."
+//         lembrar="lembrá-lo"
+//     } else {
+//         pronome="Sra."
+//         lembrar="lembrá-la"
+//     }
+//     emailCanceladas.push(`Olá, ${pronome} ${canceladas[i].nome}. Estamos enviando esta mensagem para ${lembrar} da sua consulta no dia ${canceladas[i].dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`)
+// }
+
+// for (i=0; i<confirmadas.length;i++) {
+//     if (confirmadas[i].genero==="masculino"){
+//         pronome="Sr."
+//         lembrar="lembrá-lo"
+//     } else {
+//         pronome="Sra."
+//         lembrar="lembrá-la"
+//     }
+//     emailConfirmadas.push(`Olá, ${pronome} ${confirmadas[i].nome}. Estamos enviando esta mensagem para ${lembrar} da sua consulta no dia ${confirmadas[i].dataDaConsulta}. Por favor, acuse o recebimento deste e-mail.`)
+// }
+
+// console.log(emailConfirmadas)
+// console.log(emailCanceladas)
+
+// 5
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
+
+// const novosSaldos = (conta) => {
+//     switch (conta.cliente) {
+//         case "João":
+//             conta.saldoTotal=400
+//             break;
+//         case "Paula":
+//             conta.saldoTotal=6260
+//             break;
+//         case "Pedro":
+//             conta.saldoTotal=-3340
+//             break;
+//         case "Luciano":
+//             conta.saldoTotal=-1900
+//             break;
+//         case "Artur":
+//             conta.saldoTotal=1300
+//             break;
+//         case "Soter":
+//             conta.saldoTotal=1200
+//             break;
+//     }
+
+// }
