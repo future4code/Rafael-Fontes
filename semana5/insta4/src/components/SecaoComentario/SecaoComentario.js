fb32d036144b265e466e66493de88e7f98889806
rafae <rafaelgf@gmail.com>
2020-10-16 16:44:05 -0300

    Implementação 4

--- semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -3,11 +3,12 @@ import './SecaoComentario.css'
 
 export class SecaoComentario extends Component {
 	state = {
-
+		valorInputComentario: ''
 	}
 
-	onChangeComentario() {
-
+	onChangeComentario = (event) => {
+		console.log(event.target.value)
+		this.setState({valorInputComentario: event.target.value})
 	}
 
 	render() {
@@ -15,7 +16,7 @@ export class SecaoComentario extends Component {
 			<input
 				className={'input-comentario'}
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.valorInputComentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
