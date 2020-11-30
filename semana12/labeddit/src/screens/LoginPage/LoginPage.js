880a777320e0cd337d95c5f2dd38926a0c4cf725
rafae <rafaelgf@gmail.com>
2020-11-30 14:33:45 -0300

    Signup e Login

--- semana12/labeddit/src/screens/LoginPage/LoginPage.js
+++ semana12/labeddit/src/screens/LoginPage/LoginPage.js
@@ -1,17 +1,45 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import Header from '../../components/Header/Header'
-import { goToSignUpPage } from '../../router/Coordinator'
+import { goToFeedPage, goToSignUpPage } from '../../router/Coordinator'
+import { useForm } from '../../hooks/UseForm'
+import axios from 'axios'
 
 const LoginPage = () => {
     const history = useHistory()
+    const {form, onChange, resetState} = useForm({ email: "", password: "" })
+
+    const handleInputChange = (event) => {
+        const { value, name } = event.target
+        onChange(value, name)
+    }
+  
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        const body = {
+            "email": form.email,
+            "password": form.password
+        }
+        axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/login',body)
+        .then((res)=> {
+            console.log(res)
+            localStorage.setItem('token',res.data.token)
+            localStorage.setItem('username',res.data.user.username)
+            goToFeedPage(history)
+            alert(`Bem-vindo ${res.data.user.username} Logado com sucesso.`)
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+    }
+
     return (
         <div>
             <Header />
-            <form>
-                <input type='text' value='e-mail'/>
-                <input type='text' value='password'/>
-                <button>Entrar</button>    
+            <form onSubmit={onSubmitForm}>
+                <input name='email' value={form.email} type='email' required onChange={handleInputChange}/>
+                <input name='password' value={form.password} type='password' required onChange={handleInputChange}/>
+                <button type='submit'>Entrar</button>    
             </form>
             <button onClick={()=> goToSignUpPage(history)}>Primeiro Acesso</button>
         </div>
--- semana12/labeddit/src/screens/SignUpPage/SignUpPage.js
+++ semana12/labeddit/src/screens/SignUpPage/SignUpPage.js
@@ -2,16 +2,43 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import { goToLoginPage } from '../../router/Coordinator'
+import { useForm } from '../../hooks/UseForm'
+import axios from 'axios'
 
 const SignUpPage = () => {
     const history = useHistory()
+    const {form, onChange, resetState} = useForm({ name: "", email: "", password: "" })
+
+    const handleInputChange = (event) => {
+        const { value, name } = event.target
+        onChange(value, name)
+    }
+  
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        const body = {
+            "email": form.email,
+            "password": form.password,
+            "username": form.name
+        }
+        axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/signup',body)
+        .then((res)=> {
+            goToLoginPage(history)
+            alert(`Bem-vindo ${res.data.user.username}. Acesso criado com sucesso.`)
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+    }
+
     return (
         <div>
             <Header />
-            <form>
-                <input type='text' value='e-mail'/>
-                <input type='text' value='password'/>
-                <button>Cadastrar</button>    
+            <form onSubmit={onSubmitForm}>
+                <input name='name' value={form.name} type='text' required onChange={handleInputChange}/>
+                <input name='email' value={form.email} type='email' required onChange={handleInputChange}/>
+                <input name='password' value={form.password} type='password' required onChange={handleInputChange}/>
+                <button type='submit'>Cadastrar</button>    
             </form>
             <button onClick={()=> goToLoginPage(history)}>Voltar</button>
         </div>
