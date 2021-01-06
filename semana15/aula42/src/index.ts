f2a9a9e6b267259c6108b603fd741dab129d1dde
rafae <rafaelgf@gmail.com>
2021-01-06 15:00:51 -0300

    Endpoints 2 e 3

--- semana15/aula42/src/index.ts
+++ semana15/aula42/src/index.ts
@@ -1,23 +1,74 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import {countries} from './countries'
+import { countries, country } from './countries'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003");
-});
-
 app.get('/countries/all', (req: Request, res: Response) => {
     const result = countries.map(country => ({
         id: country.id,
         name: country.name
-      }))
+    }))
       
-      res
+    res
         .status(200)
         .send(result)
 })
+
+app.get('/countries/search', (req: Request, res: Response) => {
+    let result: country[] = countries
+
+    if  (
+        !req.query.name && 
+        !req.query.capital &&
+        !req.query.continent
+        )
+    {
+        res
+        .status(422)
+        .send("No parameters")
+    } else {     
+        if (req.query.name) {
+        result = result.filter(
+            country => country.name.includes(req.query.name as string)
+        )
+        }
+
+        if (req.query.capital) {
+        result = result.filter(
+            country => country.capital.includes(req.query.capital as string)
+        )
+        }
+
+        if (req.query.continent) {
+        result = result.filter(
+            country => country.continent.includes(req.query.continent as string)
+        )
+        }
+    }
+    res
+    .status(200)
+    .send(result)
+})
+
+app.get('/countries/:id', (req: Request, res: Response) => {
+    if(isNaN(Number(req.params.id))){
+        res
+            .status(422)
+            .send("Invalid Parameter")
+    } else {
+        const result: country | undefined = countries.find(
+            country => country.id === Number(req.params.id)
+        )
+        res
+            .status(200)
+            .send(result)
+    }
+})
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
