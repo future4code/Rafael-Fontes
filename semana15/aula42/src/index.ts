575c80798e834c597a6fe538e8fb78c6a04bbbf6
rafae <rafaelgf@gmail.com>
2021-01-06 13:15:11 -0300

    Inicio

---
+++ semana15/aula42/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import {countries} from './countries'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
+
+app.get('/countries', (req: Request, res: Response) => {
+    res.send(countries)
+})
