0ef2fec47773e2ebfce948fae23dfb0e82cd6386
rafae <rafaelgf@gmail.com>
2021-01-06 17:10:12 -0300

    Endpoint 4-correção

--- semana15/aula42/src/index.ts
+++ semana15/aula42/src/index.ts
@@ -70,12 +70,21 @@ app.get('/countries/:id', (req: Request, res: Response) => {
 })
 
 app.put('/countries/edit/:id', (req: Request, res: Response) => {
-    countries[Number(req.params.id)].name = req.body.name
-    countries[Number(req.params.id)].capital = req.body.capital
-
-    res
-        .status(200)
-        .send("Alterado com sucesso")
+    const result: country | undefined = countries.find(
+        country => country.id === Number(req.params.id)
+    )
+    
+    if (result) {
+        countries[Number(req.params.id)].name = req.body.name
+        countries[Number(req.params.id)].capital = req.body.capital
+        res
+            .status(200)
+            .send("Sucessful")
+    } else {
+        res
+            .status(404)
+            .send("Not Found")
+    }
 })
 
 app.listen(3003, () => {
