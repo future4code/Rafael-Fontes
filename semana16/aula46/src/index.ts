5136a7f1148d937a93e2f2900c86ad44d016c262
rafae <rafaelgf@gmail.com>
2021-01-13 17:31:00 -0300

    Demais exercícios

--- semana16/aula46/src/index.ts
+++ semana16/aula46/src/index.ts
@@ -72,7 +72,7 @@ const updateSalaryById = async (
  }
 // updateSalaryById("007",40000)
 
- const getActorById = async (id: string): Promise<any> => {
+const getActorById = async (id: string): Promise<any> => {
    const result = await connection.raw(`
       SELECT * FROM Actor WHERE id = '${id}'
    `)
@@ -168,7 +168,85 @@ app.delete('/actor/:id', async (req:Request , res:Response) => {
     }
 })
 
+const addMovie = async (
+   id: string,
+   title: string,
+   synopsis: string,
+   launch_date: Date,
+   rating: number,
+   playing_limit_date: Date
+ ): Promise<void> => {
+   await connection
+     .insert({
+      id: id,
+      title: title,
+      synopsis: synopsis,
+      launch_date: launch_date,
+      rating: rating,
+      playing_limit_date: playing_limit_date
+     })
+     .into("Movie")
+}
 
+app.post('/movie', async (req: Request, res: Response) => {
+   try {
+      await addMovie(
+         req.body.id,
+         req.body.title,
+         req.body.synopsis,
+         req.body.launch_date,
+         req.body.rating,
+         req.body.playing_limit_date
+       )
+       res.status(200).send("Filme adicionado");
+   } catch (err) {
+      res.status(400).send({
+         message: err.message,
+       })
+   }
+})
+
+const getAllMovies = async (): Promise<any> => {
+   const result = await connection.raw(`
+      SELECT * FROM Movie LIMIT 15
+   `)
+   
+   return result[0]
+}
+
+app.get('/movie/all', async (req:Request , res:Response) => {
+   try {
+      const movies = await getAllMovies();
+      res.status(200).send(movies)
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+})
+
+const searchMovies = async (query: string): Promise<any> => {
+   const result = await connection.raw(`
+      SELECT title, synopsis, launch_date FROM Movie
+      WHERE
+         (title LIKE "%${query}%"
+         OR
+         synopsis LIKE "%${query}%")
+      ORDER BY launch_date DESC
+   `)
+   return result[0]
+}
+
+app.get('/movie/search', async (req:Request , res:Response) => {
+   try {
+      const result = await searchMovies(req.query.searchQuery as string);
+      res.status(200).send({result})
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+})
 
 // const pokemons: Pokemon[] = []
 
