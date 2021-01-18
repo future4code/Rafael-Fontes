b522d91302ce2724812a461d8e255234d3d5ab07
rafae <rafaelgf@gmail.com>
2021-01-18 18:41:20 -0300

    Valor padrão offset

--- semana17/aula48/src/endpoints/getUsersByFilters.ts
+++ semana17/aula48/src/endpoints/getUsersByFilters.ts
@@ -3,19 +3,26 @@ import selectUsersByFilters from '../data/selectUsersByFilters'
 
 export const getUsersByFilters = async(req: Request,res: Response): Promise<any> =>{
     try {
-       const userName = req.query.name as string
-       const type = req.query.type as string
-       const orderBy = req.query.orderBy as string
-       const limit = req.query.limit as string
-       const offset = req.query.offset as string
-       const users = await selectUsersByFilters(userName, type, orderBy, limit, offset)
-       
-       if(!users.length){
-          res.statusCode = 404
-          throw new Error("Nenhum usuário encontrado")
-       }
- 
-       res.status(200).send(users)
+      const userName = req.query.name as string
+      const type = req.query.type as string
+      const orderBy = req.query.orderBy as string
+      const limit = req.query.limit as string
+      let offset = req.query.offset as string
+
+      if (!req.query.offset) {
+         offset  = '0'
+      } else {
+         offset = req.query.offset as string
+      }
+
+      const users = await selectUsersByFilters(userName, type, orderBy, limit, offset)
+      
+      if(!users.length){
+         res.statusCode = 404
+         throw new Error("Nenhum usuário encontrado")
+      }
+
+      res.status(200).send(users)
        
     } catch (error) {
        console.log(error)
