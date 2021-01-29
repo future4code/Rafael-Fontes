8534db3bcf02538c2c1757deb61d8f9d74bdbc84
rafae <rafaelgf@gmail.com>
2021-01-29 13:20:22 -0300

    correção data no feed

--- semana18/cookenu/src/endpoints/getFeed.ts
+++ semana18/cookenu/src/endpoints/getFeed.ts
@@ -8,9 +8,21 @@ export const getFeed = async(req: Request,res: Response): Promise<any> =>{
         const token = req.headers.authorization as string
         const authenticationData = getData(token)
         const user_id = await getUserById(authenticationData.id)
-        const recipes = await selectRecipes(
+        const feed = await selectRecipes(
             user_id.id
         )
+
+        var dayjs = require('dayjs')
+        const recipes = feed.map((recipe: { id: string; title: string; description: string; createdAt: string; userId: string; userName: string  }) => {
+            return {
+                 id: recipe.id,
+                 title: recipe.title,
+                 description: recipe.description,
+                 createdAt: dayjs(recipe.createdAt).format('DD/MM/YYYY'),
+                 userId: recipe.userId,
+                 userName: recipe.userName
+            }
+        })
 
         res.status(200).send({recipes})
 
