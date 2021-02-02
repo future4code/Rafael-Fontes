503267c8c8f6520f49765f8ae592b45963bd11cc
rafae <rafaelgf@gmail.com>
2021-02-02 14:50:38 -0300

    Ex 1

---
+++ semana19/aula55/src/data/routes/taskRouter.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { createTask, getTaskById } from "../../controller/taskController";
+
+export const taskRouter = express.Router()
+
+taskRouter.put("/", createTask)
+taskRouter.get("/:id", getTaskById)
