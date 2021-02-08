6fc19f9d317f47040814411809da61f98deabdf4
rafae <rafaelgf@gmail.com>
2021-02-08 18:46:21 -0300

    Ex 5b

--- semana20/aula58/tests/index.test.ts
+++ semana20/aula58/tests/index.test.ts
@@ -74,8 +74,6 @@ describe("Exercício 5", () => {
 		  nacionality: NACIONALITY.BRAZILIAN,
 		}
 	
-		const name = brazilian.name
-
 		const casino: Casino = {
 		  name: "Casino",
 		  location: LOCATION.BRAZIL,
@@ -85,4 +83,21 @@ describe("Exercício 5", () => {
 		expect(result.brazilians.allowed.length).toBeGreaterThan(0)
 		expect(result.brazilians.allowed.length).toBeLessThan(2)
 	})
+
+	test("Teste (b)", () => {
+		const american: User_ex3 = {
+		  name: "Fulano",
+		  age: 18,
+		  nacionality: NACIONALITY.AMERICAN,
+		}
+	
+		const casino: Casino = {
+		  name: "Casino",
+		  location: LOCATION.BRAZIL,
+		}
+	
+		const result = verifyAge(casino, [american])
+		expect(result.americans.unallowed.length).toBe(0)
+	})
+
 })
